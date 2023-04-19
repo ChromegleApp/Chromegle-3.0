230c15c759662608ea4b66f94acc97d46ad4a308
isaac <isaacikogan@gmail.com>
2023-04-18 20:48:31 -0400

    Fix improper binding of listeners & poor type hinting of modules in loadModules

--- src/core/chromegle/modules.ts
+++ src/core/chromegle/modules.ts
@@ -1,39 +1,30 @@
+import {Logger} from "./logging";
+
 /**
  * Definition of a Chromegle "Module". Modules are the core of Chromegle functionality.
  * Modules are defined by extending this class and managed by the extension.
  */
-import {Logger} from "./logging";
-
 export abstract class Module {
 
     abstract id: string;
 
-
-
-    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
-        (listener as CallableFunction).bind(this);
-        document.addEventListener(type, listener, options);
-
-        let element = document.createElement("div");
-        element.querySelector("");
-
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions, element?: HTMLElement): void {
+        listener = (listener as CallableFunction).bind(this) as EventListener;
+        if (element === null) throw new DOMException("Element with \"null\" value passed to event listener");
+        (element || document).addEventListener(type, listener, options);
     }
 
-    addOmegleEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
-        this.addEventListener(`omegle.${type}`, listener, options)
+    addOmegleEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions, element?: HTMLElement): void {
+        this.addEventListener(`omegle.${type}`, listener, options, element);
     }
 
-    addChromegleEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
-        this.addEventListener(`chromegle.${type}`, listener, options)
+    addChromegleEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions, element?: HTMLElement): void {
+        this.addEventListener(`chromegle.${type}`, listener, options, element);
     }
-
-
-
-
 
 }
 
-export function loadModules<T extends Module>(...modules: Array<new() => T>): Array<Module> {
+export function loadModules(...modules: Array<new() => Module>): Array<Module> {
     const instances: Array<Module> = [];
 
     for (let module of modules) {
